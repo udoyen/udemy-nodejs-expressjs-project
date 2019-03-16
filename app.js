bf302c6c65cd2ee9bbb6cebe0ce1f851b8dc5e1a
udoyen <datameshprojects@gmail.com>
2019-03-16 19:46:15 +0100

    pug files done.

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(shopRoutes);
 
 // Catch all middleware
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
 })
 
 app.listen(3000);
