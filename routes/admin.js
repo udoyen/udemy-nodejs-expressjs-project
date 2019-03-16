6c33d6d6d7aeccf0a9ff2ce82bb4a73fd91ad2a0
udoyen <datameshprojects@gmail.com>
2019-03-16 17:58:50 +0100

    added new files

--- routes/admin.js
+++ routes/admin.js
@@ -1,21 +1,20 @@
 const path = require('path');
+
 const express = require('express');
 
-// Import the path.js module
-const rootDir = require('../utils/path');
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-
-})
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+});
 
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-})
+  console.log(req.body);
+  res.redirect('/');
+});
 
 module.exports = router;
