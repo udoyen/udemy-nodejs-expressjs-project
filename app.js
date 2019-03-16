fdcae32a40d83b0455f6112c962747320254f847
udoyen <datameshprojects@gmail.com>
2019-03-16 11:01:09 +0100

    basic routes setup

--- app.js
+++ app.js
@@ -1,21 +1,38 @@
 const http = require('http');
+const path = require('path');
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const defaultRoute = require('./routes/shop');
+
+// Register a parser
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/admin', adminRoutes);
+
+app.use(defaultRoute);
+
 // Used to add middleware
 // next is a function
+// app.use((req, res, next) => {
+//     console.log('In the middleware here');
+//     next(); // Allows the request to continue to the next middlwware in line
+
+// })
+
+// app.use('/', (req, res, next) => {
+//     console.log('In first middleware');
+//     next();    
+
+// })
+
+// Catch all middleware
 app.use((req, res, next) => {
-    console.log('In the middleware here');
-    next(); // Allows the request to continue to the next middlwware in line
-
-})
-
-app.use((req, res, next) => {
-    console.log('In another middleware here');
-    res.send('<h1>Hello from express</h1>');
-
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 })
 
 app.listen(3000);
