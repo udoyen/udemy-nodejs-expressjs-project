75368723bc215fdd2c08987610cd1a608e281b8d
udoyen <datameshprojects@gmail.com>
2019-03-22 19:52:11 +0100

    setup ejs template

--- app.js
+++ app.js
@@ -7,14 +7,14 @@ const expressHbs = require('express-handlebars');
 const app = express();
 
 // Setup Handlebars
-app.engine('hbs', expressHbs({
-    layoutsDir: 'views/layouts/',
-     defaultLayout: 'main-layout',
-     extname: 'hbs'
-    }));
+// app.engine('hbs', expressHbs({
+//     layoutsDir: 'views/layouts/',
+//      defaultLayout: 'main-layout',
+//      extname: 'hbs'
+//     }));
 
 // Set global configuration value
-app.set('view engine',  'hbs');
+app.set('view engine',  'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
@@ -45,7 +45,10 @@ app.use(shopRoutes);
 
 // Catch all middleware
 app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page Not Found'});
+    res.status(404).render('404', {
+        pageTitle: 'Page Not Found',
+        path: " " // stops the 404 page from complaining
+    });
 })
 
 app.listen(3000);
