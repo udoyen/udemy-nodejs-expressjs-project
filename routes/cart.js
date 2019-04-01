27f44283cc5f349d9bbaf6101767ab823d9f162a
udoyen <datameshprojects@gmail.com>
2019-04-01 18:16:26 +0100

    order setup done.

--- routes/cart.js
+++ routes/cart.js
@@ -9,5 +9,6 @@ const router = express.Router();
 router.get("/cart", shopController.getCart);
 router.post('/cart', shopController.postCart);
 router.post('/cart-delete-item', shopController.postCartDeleteProduct);
+router.post('/create-order', shopController.postOrder);
 
 module.exports = router;
