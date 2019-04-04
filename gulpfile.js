5f6843c74a69bcb31f182a32b6b6bff9217495bb
udoyen <datameshprojects@gmail.com>
2019-04-04 11:16:38 +0100

    downgrading

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,20 @@
 var gulp = require("gulp");
 var browserSync = require("browser-sync").create();
 
-gulp.task('serve', () => {
+gulp.task("serve", () => {
   browserSync.init({
-    proxy: 'http://localhost:3000',
-    files: "./data/*json"
+    proxy: "http://localhost:3000",
+    files: [
+      "./controller/*.js",
+      "./models/*.js",
+      "./routes/*.js",
+      "./utils/*.js",
+      "./views/admin/*.ejs",
+      "./views/includes/*.ejs",
+      "./views/shop/*.ejs",
+      "./public/css/*.css",
+      "./public/js/*.js"
+    ]
   });
 
   // gulp.watch('./data/*.json').on('change', browserSync.reload);
