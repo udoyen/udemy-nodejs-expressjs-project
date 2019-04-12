c18e1cd391de21b7ac2be4ce552ffcbc7e723793
udoyen <datameshprojects@gmail.com>
2019-04-12 18:07:06 +0100

    removed console function in auth file

--- controllers/auth.js
+++ controllers/auth.js
@@ -14,7 +14,6 @@ exports.postLogin = (req, res, next) => {
         .then(user => {
             req.session.isLoggedIn = true;
             req.session.user = user;
-            console.log(user);
             req.session.save(err => {
                 console.log(err);
                 res.redirect('/');
