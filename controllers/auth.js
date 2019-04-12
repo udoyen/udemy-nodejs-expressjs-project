6f8ab7d377a20719524ca310971736a811c65527
udoyen <datameshprojects@gmail.com>
2019-04-12 12:11:56 +0100

    added session deletion

--- controllers/auth.js
+++ controllers/auth.js
@@ -14,11 +14,16 @@ exports.postLogin = (req, res, next) => {
         .then(user => {
             req.session.isLoggedIn = true;
             req.session.user = user;
-        })
-        .then(() => {
             res.redirect('/');
         })
         .catch(err => {
             console.log(err);
         });
+}
+
+exports.postLogout = (req, res, next) => {
+    req.session.destroy(err => {
+        console.log(err);
+        res.redirect('/');
+    });
 }
