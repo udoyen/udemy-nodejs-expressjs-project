6353ecad195a616288d3cf5dee60c3406e9262b2
udoyen <datameshprojects@gmail.com>
2019-04-15 10:43:17 +0100

    initial signup setup

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,6 +8,16 @@ exports.getLogin = (req, res, next) => {
     });
 };
 
+
+exports.getSignup = (req, res, next) => {
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
+    isAuthenticated: false
+  });
+};
+
+
 exports.postLogin = (req, res, next) => {
     // set a header 
     User.findById('5ca795ae329d5d22945c662a')
@@ -21,6 +31,10 @@ exports.postLogin = (req, res, next) => {
         })
         .catch(err => console.log(err));
 };
+
+
+exports.postSignup = (req, res, next) => {};
+
 
 exports.postLogout = (req, res, next) => {
     req.session.destroy(err => {
