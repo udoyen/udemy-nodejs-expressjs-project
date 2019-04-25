6cf8c3c4b867a9befccdba37c41c6b198fbbe982
udoyen <datameshprojects@gmail.com>
2019-04-25 18:11:08 +0100

    flash error messages added

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 const csurf = require('csurf');
 const csrfProtection = csurf();
+const flash = require('connect-flash');
 const User = require("./models/user");
 const MONGODB_URI =
   'mongodb+srv://george:udemy_321@udemycluster-bb3gw.mongodb.net/shop';
@@ -39,6 +40,7 @@ app.use(
   })
 );
 app.use(csrfProtection);
+app.use(flash());
 
 app.use((req, res, next) => {
   if (!req.session.user) {
--- controllers/auth.js
+++ controllers/auth.js
@@ -2,19 +2,31 @@ const bcrypt = require('bcryptjs');
 const User = require("../models/user");
 
 exports.getLogin = (req, res, next) => {
+    let message = req.flash('error');
+    if (message.length > 0) {
+        message = message[0];
+    } else {
+        message = null;
+    }
     res.render('auth/login', {
         path: '/login',
         pageTitle: 'Login',
-        isAuthenticated: false
+        errorMessage: message
     });
 };
 
 
 exports.getSignup = (req, res, next) => {
+    let message = req.flash('error');
+    if (message.length > 0) {
+        message = message[0];
+    } else {
+        message = null;
+    }
     res.render('auth/signup', {
         path: '/signup',
         pageTitle: 'Signup',
-        isAuthenticated: false
+        errorMessage: message
     });
 };
 
@@ -28,6 +40,7 @@ exports.postLogin = (req, res, next) => {
     })
         .then(user => {
             if (!user) {
+                req.flash('error', 'Invalid email or password.');
                 return res.redirect('/login');
             }
             bcrypt
@@ -41,6 +54,8 @@ exports.postLogin = (req, res, next) => {
                             res.redirect('/');
                         });
                     }
+
+                    req.flash('error', 'Invalid email or password.');
                     res.redirect('/login');
                 })
                 .catch(err => {
@@ -60,6 +75,7 @@ exports.postSignup = (req, res, next) => {
     User.findOne({ email: email })
         .then(userDoc => {
             if (userDoc) {
+                req.flash('error', 'Error Signing Up, E-Mail exists already!');
                 return res.redirect('/signup');
             }
             return bcrypt.hash(password, 12)
