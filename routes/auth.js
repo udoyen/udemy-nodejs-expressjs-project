8a5a3f3f0c991b43bf7ef65734645dc532af0c61
udoyen <datameshprojects@gmail.com>
2019-04-29 16:21:18 +0100

    token sending done

--- routes/auth.js
+++ routes/auth.js
@@ -15,4 +15,7 @@ router.post('/signup', authController.postSignup);
 
 router.post('/logout', authController.postLogout);
 
+router.get('/reset', authController.getReset);
+router.post('/reset', authController.postReset);
+
 module.exports = router;
