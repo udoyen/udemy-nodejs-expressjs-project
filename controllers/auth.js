8559975d400f0367e714d31ee6fed7c820a6aa53
udoyen <datameshprojects@gmail.com>
2019-04-29 17:53:55 +0100

    password reset done

--- controllers/auth.js
+++ controllers/auth.js
@@ -178,4 +178,76 @@ exports.postReset = (req, res, next) => {
                 console.log(err);
             });
     })
-}
+};
+
+exports.getNewPassword = (req, res, next) => {
+    const token = req.params.token;
+    User.findOne(
+        {
+            resetToken: token,
+            resetTokenExpiration: {
+                $gt: Date.now()
+            }
+        })
+        .then(user => {
+            let message = req.flash('error');
+            if (message.length > 0) {
+                message = message[0];
+
+            } else {
+                message = null;
+            }
+            res.render('auth/new-password', {
+                path: '/new-password',
+                pageTitle: 'New Password',
+                errorMessage: message,
+                userId: user._id.toString(),
+                passwordToken: token
+            });
+
+        })
+        .catch(err => {
+            console.log(err);
+        })
+};
+
+exports.postNewPassword = (req, res, next) => {
+    const newPassword = req.body.password;
+    const userId = req.body.userId;
+    const passwordToken = req.body.passwordToken;
+    let resetUser;
+
+    User.findOne({
+        resetToken: passwordToken,
+        resetTokenExpiration: { $gt: Date.now() },
+        _id: userId
+    })
+        .then(user => {
+            resetUser = user;
+            // TODO: add the function to 
+            // prevent user from using old password
+            return bcrypt.hash(newPassword, 12);
+        })
+        .then(hashPassword => {
+            resetUser.password = hashPassword;
+            resetUser.resetToken = undefined;
+            resetUser.resetTokenExpiration = undefined;
+            return resetUser.save();
+        })
+        .then(result => {
+            res.redirect('/login');
+            transporter.sendMail({
+                to: resetUser.email,
+                from: 'shop@node-complete.com',
+                subject: 'Password Reset',
+                html: `
+                        <p>Your password was reset!</p>
+                    
+                    `
+            })
+
+        })
+        .catch(err => {
+            console.log(err);
+        })
+};
